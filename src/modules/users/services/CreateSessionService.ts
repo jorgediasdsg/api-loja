eccfbcfdd12f9c20274612ba29c091884a807fee
Jorge Dias <jorge.dias@masonweb.com.br>
2021-07-18 11:14:21 -0300

    ADD: jwt

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,5 +1,6 @@
 import AppError from '@shared/errors/AppError';
 import { compare, hash } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -9,8 +10,13 @@ interface IRequest {
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -24,7 +30,15 @@ class CreateSessionService {
       throw new AppError('Incorrect Email/Password.', 401);
     }
 
-    return user;
+    const token = sign({}, '521821dcb7aa85b70f6760058db6eda1881fca55', {
+      subject: user.id,
+      expiresIn: '1h',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
