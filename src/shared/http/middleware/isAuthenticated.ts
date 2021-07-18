ff240ff4e62c7aace0b9fa213014f03c5631d13a
Jorge Dias <jorge.dias@masonweb.com.br>
2021-07-18 14:49:48 -0300

    ADD: Middleware Authenticated pass user id to next

--- src/shared/http/middleware/isAuthenticated.ts
+++ src/shared/http/middleware/isAuthenticated.ts
@@ -3,6 +3,12 @@ import AppError from '@shared/errors/AppError';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -17,6 +23,13 @@ export default function isAuthenticated(
 
   try {
     const decodeToken = verify(token, auth.jwt.secret);
+
+    const { sub } = decodeToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch {
     throw new AppError('Invalid JWT Token');
