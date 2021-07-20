b23a321acb89931593efb993b1c665ef15b92736
Jorge Dias <jorge.dias@masonweb.com.br>
2021-07-20 17:20:33 -0300

    ADD: Customers ORM/Controller/Services

---
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateCustomerService {
+  public async execute({ id, name, email }: IRequest): Promise<Customer> {
+    const customerRepository = getCustomRepository(CustomersRepository);
+
+    const customer = await customerRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('customer not found.');
+    }
+
+    const customerUpdateEmail = await customerRepository.findByEmail(email);
+
+    if (customerUpdateEmail && customerUpdateEmail.id !== customer.id) {
+      throw new AppError('Email already exists.');
+    }
+
+    customer.name = name;
+    customer.email = email;
+
+    await customerRepository.save(customer);
+
+    return customer;
+  }
+}
+
+export default UpdateCustomerService;
